25faf037cefaa0f99ded9b4d97e60bb51a3ac09f
Ahmed <ahmedalharbi85@icloud.com>
2021-05-03 04:38:51 +0300

    Initial Commit

---
+++ Nearby-Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nearby-Places
+//
+//  Created by Ahmed Alharbi on 21/09/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nearby-Places/Nearby_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nearby_PlacesApp.swift
+//  Nearby-Places
+//
+//  Created by Ahmed Alharbi on 21/09/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct Nearby_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
